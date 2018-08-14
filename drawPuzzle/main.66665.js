7f868e64b3d1fb799844fab5a99ea2fbc3707546
FuBei_M <fubei1989@163.com>
2018-08-14 23:54:54 +0800

    最新版本；增加黑线

--- drawPuzzle/main.66665.js
+++ drawPuzzle/main.66665.js
@@ -135,7 +135,7 @@ window.boot = function () {
         BK.Script.loadlib();
     }
     else {
-        var bundledScript = settings.debug ? 'src/project.dev.js' : 'src/project.28d1e.js';
+        var bundledScript = settings.debug ? 'src/project.dev.js' : 'src/project.63964.js';
         if (jsList) {
             jsList = jsList.map(function (x) {
                 return 'src/' + x;
@@ -166,7 +166,7 @@ window.boot = function () {
     cc.game.run(option, onStart);
 };
 
-// main.c6458.js is qqplay and jsb platform entry file, so we must leave platform init code here
+// main.66665.js is qqplay and jsb platform entry file, so we must leave platform init code here
 if (false) {
     BK.Script.loadlib('GameRes://src/settings.js');
     BK.Script.loadlib();
@@ -190,7 +190,7 @@ if (false) {
     window.boot();
 }
 else if (window.jsb) {
-    require('src/settings.9cefa.js');
+    require('src/settings.86567.js');
     require('src/cocos2d-jsb.js');
     require('jsb-adapter/engine/index.js');
     window.boot();
